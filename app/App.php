37ab646fb92978aae942ebadab1da2b132ab0d8e
hannesroegiers <hannes.roegiers@student.hogent.be>
2023-09-17 12:59:10 +0200

    base functionality finished

--- app/App.php
+++ app/App.php
@@ -2,10 +2,73 @@
 
 declare(strict_types = 1);
 
-function getTransactionFiles(): array
+function getTransactionFiles(string $dirPath): array
 {
     $files = [];
-    foreach(scandir(FILES_PATH) as $file) {
-        var_dump($file);
+
+    foreach(scandir($dirPath) as $file) {
+        if (is_dir($file)){
+            continue;
+        }
+        $files[] = $dirPath . $file;
     }
+    return $files;
+}
+
+function getTransactions(string $filename, ?callable $transactionHandler = null): array
+{
+    if (!file_exists($filename))
+    {
+        trigger_error('File "' . $filename . '" does not exist. ', E_USER_ERROR);
+    }
+    $file = fopen($filename, 'r');
+
+    fgetcsv($file);
+
+    $transactions =[];
+
+    while (($transaction = fgetcsv($file)) != false)
+    {
+        if ($transactionHandler !== null)
+        {
+            $transaction = $transactionHandler($transaction);
+        }
+        $transactions[] = $transaction;
+    }
+
+    return $transactions;
+}
+
+function extractTransaction(array $transactionRow): array
+{
+    [$date, $checkNumber, $description, $amount] = $transactionRow;
+
+    $amount = (float) str_replace(['$', ','], '', $amount);
+
+    return[
+        'date'        => $date,
+        'checkNumber' => $checkNumber,
+        'description' => $description,
+        'amount'      => $amount
+    ];
+}
+
+function calculateTotals(array $transactions): array
+{
+    $totals = ['netTotal' => 0, 'totalIncome' => 0, 'totalExpense' => 0];
+
+    foreach ($transactions as $transaction)
+    {
+        $totals['netTotal'] += $transaction['amount'];
+
+        if ($transaction['amount'] >= 0)
+        {
+            $totals['totalIncome'] += $transaction['amount'];
+        }
+        else
+        {
+            $totals['totalExpense'] += $transaction['amount'];
+        }
+    }
+    return $totals;
 }
--- public/index.php
+++ public/index.php
@@ -9,5 +9,16 @@ define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 require APP_PATH . "App.php";
+require  APP_PATH . "helpers.php";
 
-getTransactionFiles();
+
+$files = getTransactionFiles(FILES_PATH);
+
+$transactions = [];
+foreach($files as $file)
+{
+    $transactions = array_merge($transactions, getTransactions($file, 'extractTransaction'));
+}
+
+$totals = calculateTotals($transactions);
+require VIEWS_PATH . 'transactions.php';
